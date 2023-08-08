1ade156891b078beea287d36d1f0e8299ee10f6b
Alanm17 <mukhammadazizkholdarov@gmail.com>
2023-08-08 16:38:31 +0900

    ..

---
+++ script.js
@@ -0,0 +1,9 @@
+let js = "amazing";
+if (js === "amazing") alert("Java script is fun");
+console.log(50 - 20 + 24);
+
+console.log("jonas");
+console.log(23);
+
+let firstName = "jonas";
+console.log(firstName);
