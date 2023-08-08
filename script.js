b026726d3f930069bd7467e1914b747591ca479d
Alanm17 <mukhammadazizkholdarov@gmail.com>
2023-08-09 01:35:29 +0900

    ..

--- script.js
+++ script.js
@@ -1,21 +1,76 @@
-let js = "amazing";
-if (js === "amazing") alert("Java script is fun");
-console.log(50 - 20 + 24);
+// let js = "amazing";
+// if (js === "amazing") alert("Java script is fun");
+// console.log(50 - 20 + 24);
 
-console.log("jonas");
-console.log(23);
+// console.log("jonas");
+// console.log(23);
 
-let firstName = "Matilda";
+// let firstName = "Matilda";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let jonas_matilda = "JM";
-let $function = 27;
+// // Variable name convertions
 
-let myFirstJob = "programmer";
-let myCurrentJob = "Teacher";
+// let jonas_matilda = "JM";
+// let $function = 27;
 
-console.log(myFirstJob);
-console.log(myCurrentJob);
+// let myFirstJob = "programmer";
+// let myCurrentJob = "Teacher";
+
+// console.log(myFirstJob);
+// console.log(myCurrentJob);*/
+
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "jonas");
+
+javascriptIsFun = "yes!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+// let age = 30;
+// age = 31;
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// // const job;
+// var job = "programmer";
+// job = "teacher";
+
+// lastName = "kholdarov";
+// console.log(lastName);
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Schmedtman";
+console.log(firstName + " " + lastName);
+// Assignment operators
+let x = 10 + 5;
+(x += 10), (x *= 4);
+x++;
+x--;
+x--;
+console.log(x);
+
+//Comparison operator
+console.log(ageJonas > ageSarah);
