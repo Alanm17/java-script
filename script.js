5c5333d019f94b89a68ac9d17c6361e9e6b4bb88
Alanm17 <mukhammadazizkholdarov@gmail.com>
2023-08-08 16:54:11 +0900

    ..

--- script.js
+++ script.js
@@ -5,5 +5,17 @@ console.log(50 - 20 + 24);
 console.log("jonas");
 console.log(23);
 
-let firstName = "jonas";
+let firstName = "Matilda";
+
 console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let myFirstJob = "programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
